1955e7fcc4835b03e909d6e5d1af7d8f97125e83
tuyara grigoreva <tuyagrig@gmail.com>
2023-07-05 22:22:54 +0300

    feat: code upload

---
+++ Day01/main.js
@@ -0,0 +1,19 @@
+
+let firstName = 'Asabeneh';
+let lastName = 'Yetayeh';
+let country = 'Finland';
+let city = 'Helsinki';
+let age = 100;
+let isMarried = true;
+
+console.log(firstName, lastName, country, city, age, isMarried);
+
+const gravity = 9.81;
+const boilingPoint = 100;
+const PI = 3.14;
+
+console.log(gravity, boilingPoint, PI);
+
+let job = 'teacher';
+
+console.log(firstName, job, country);
